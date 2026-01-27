da62518822b6353fa6d0ade303459f7f05883975
Gustavo <gustavojohan20@gmail.com>
2026-01-27 01:13:16 -0300

    Melhora o codigo do problema 1046

--- iniciante/1046.py
+++ iniciante/1046.py
@@ -1,9 +1,3 @@
 h_i, h_f = map(int, input().split())
-
-if h_i > h_f:
-    duracao = (12 + h_f) - (h_i - 12)
-    print(f'O JOGO DUROU {duracao} HORA(S)')
-elif h_i == h_f:
-    print('O JOGO DUROU 24 HORA(S)')
-else:
-    print(f'O JOGO DUROU {h_f - h_i} HORA(S)')
+horas = (24 + h_f - h_i) if h_i >= h_f else h_f - h_i
+print(f"O JOGO DUROU {int(horas)} HORA(S)")
