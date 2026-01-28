34806e86bc712060d9dae8c26a726c805e924460
Gustavo <gustavojohan200@gmail.com>
2026-01-28 20:40:46 -0300

    Corrige o codigo do 1094.py

--- iniciante/1094.py
+++ iniciante/1094.py
@@ -6,12 +6,12 @@ for i in range(qnt_testes):
     qnt_cobaias, tipo_cobaia = input().split()
     qnt_cobaias = int(qnt_cobaias)
     cobaias[tipo_cobaia] += qnt_cobaias
-    cobaias["Total"] += qnt_cobaias
+    cobaias['Total'] += qnt_cobaias
 
-print(f"Total: {cobaias["Total"]} cobaias")
-print("Total de coelhos: ", cobaias["C"])
-print("Total de ratos: ", cobaias["R"])
-print("Total de sapos: ", cobaias["S"])
-print(f"Percentual de coelhos: {cobaias['C']/cobaias["Total"]:.2f} %")
-print(f"Percentual de ratos: {cobaias['R']/cobaias["Total"]:.2f} %")
-print(f"Percentual de sapos: {cobaias['S']/cobaias["Total"]:.2f} %")
+print(f"Total: {cobaias['Total']} cobaias")
+print("Total de coelhos:", cobaias["C"])
+print("Total de ratos:", cobaias["R"])
+print("Total de sapos:", cobaias["S"])
+print(f"Percentual de coelhos: {(cobaias['C']/cobaias['Total'])*100:.2f} %")
+print(f"Percentual de ratos: {(cobaias['R']/cobaias['Total'])*100:.2f} %")
+print(f"Percentual de sapos: {(cobaias['S']/cobaias['Total'])*100:.2f} %")
